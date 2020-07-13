54ff5b383703c1e90c157cccbce92f2f5fc3f48a
Alexander Bespoyasov <forjob159@gmail.com>
2020-07-13 13:39:18 +0300

    Add page titles to all the pages

--- pages/heroku.tsx
+++ pages/heroku.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Head from "next/head";
 
 const Heroku = () => {
   return (
     <main>
+      <Head>
+        <title>Deploying on Heroku</title>
+      </Head>
+
       <h1>Deploying on Heroku</h1>
     </main>
   );
--- pages/vercel.tsx
+++ pages/vercel.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Head from "next/head";
 
 const Vercel = () => {
   return (
     <main>
+      <Head>
+        <title>Deploying on Vercel</title>
+      </Head>
+
       <h1>Deploying on Vercel</h1>
     </main>
   );
